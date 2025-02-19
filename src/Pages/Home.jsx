7771e0b15faa51f8820e0827056c9a2fd3a0166b
taseen17 <muktadirtaseen98@gmail.com>
2025-02-20 01:41:25 +0600

    services done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Slider from '../components/Slider';
 import Finance from '../components/Finance';
 import Faq from '../components/Faq';
+import Services from '../components/Services';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <Finance></Finance>
+            <Services></Services>
             <Faq></Faq>
         </div>
     );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto p-7 bg-emerald-50 rounded-lg mt-10'>
+            <h2 className='text-4xl text-center font-semibold mb-7'>Our Services</h2>
+            <div className='grid lg:grid-cols-3 gap-3 md:grid-cols-2 sm:grid-cols-1'>
+                {
+                    services.map(service =>
+                        <div className="card card-compact w-96 shadow-xl">
+                            <figure>
+                                <img className='w-full h-52'
+                                    src={service.image}
+                                    alt="Shoes" />
+                            </figure>
+                            <div className="card-body bg-emerald-50">
+                                <h2 className="card-title">{service.service_name}</h2>
+                                <p className='font-medium text-base'>Category: {service.category}</p>
+                                <p className='font-medium text-base'>Price: {service.pricing}</p>
+                                <p className='font-medium text-base'>Counselor: {service.counselor}</p>
+                                <div className="card-actions justify-end mt-2">
+                                    <button className="btn bg-emerald-600 text-white">Learn More</button>
+                                </div>
+                            </div>
+                        </div>)
+                }
+            </div>
+
         </div>
     );
 };
